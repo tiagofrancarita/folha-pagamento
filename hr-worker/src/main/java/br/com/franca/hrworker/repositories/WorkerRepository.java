dba1b83d3b88b854451dba114d371167e97cafef
Tiago França <tiagofranca.rita@gmail.com>
2023-09-15 01:18:52 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/br/com/franca/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,13 @@
+package br.com.franca.hrworker.repositories;
+
+import br.com.franca.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+
+
+
+
+
+}
