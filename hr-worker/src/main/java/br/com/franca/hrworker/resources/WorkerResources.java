99656637e36f629a617d6fd48335cb79ab33f7d2
Tiago França <tiagofranca.rita@gmail.com>
2023-09-16 18:17:50 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/br/com/franca/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/br/com/franca/hrworker/resources/WorkerResources.java
@@ -41,6 +41,12 @@ public class WorkerResources {
     @GetMapping(value = "/findById/{id}")
     public ResponseEntity<Worker> findById(@PathVariable  Long id){
 
+      /*  try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
         logger.info("Port:" + env.getProperty("local.server.port"));
 
         Worker worker = workerRepository.findById(id).get();
