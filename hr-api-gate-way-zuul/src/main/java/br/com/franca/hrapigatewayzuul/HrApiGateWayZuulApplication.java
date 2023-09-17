98a48f45b63a0e6ef6a53ebbe00ecd3c5b25fc38
Tiago França <tiagofranca.rita@gmail.com>
2023-09-16 22:04:59 -0300

    Project hr api gateway

---
+++ hr-api-gate-way-zuul/src/main/java/br/com/franca/hrapigatewayzuul/HrApiGateWayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGateWayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGateWayZuulApplication.class, args);
+    }
+
+}
